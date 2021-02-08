9bc0f271f53c0e782b7dec7fd7b8d6a2c2682771
lenox139st <lenox139st@gmail.com>
2021-02-08 17:57:53 +0100

    builder / filter update

--- src/main/java/com/project/dp/Filter/Filter.java
+++ src/main/java/com/project/dp/Filter/Filter.java
@@ -9,7 +9,7 @@ public class Filter {
 
     static Set<String> KEY_TABLES = new HashSet<>(Arrays.asList("acl", "users", "roles"));
 
-    private static String delsemicolon(String s) {
+    private String delSemicolon(String s) {
         int l = s.length();
         if (s.charAt(l-1)==';') {
             return s.substring(0,l-1);
@@ -18,38 +18,82 @@ public class Filter {
             return s;
         }
     }
-    private static List<String> clearList(List<String> l) {
+    static List<String> clearList(List<String> l) {
         l.removeIf(i -> i.equals("") || i.equals(" "));
         return l;
     }
+    private boolean ifComma(String s) {
+        int l = s.length();
+        return s.charAt(l - 1) == ',';
+    }
 
-    public static String filter(String q, Long user_id) {
+    public String filter(String q, Long user_id) {
         QueryBuilder qb = new QueryBuilder();
-        List<String> initwordList = new ArrayList<String>(Arrays.asList((q.split(" ")).clone()));
+        List<String> initwordList = new ArrayList<>(Arrays.asList((q.split(" ")).clone()));
         List<String> wordList = clearList(initwordList);
         int size = wordList.size();
-        boolean prevTab = false;
+        boolean prevFor = false;
         if (size>0) {
             qb.buildFirstElement(wordList.get(0));
             for (int i = 1; i < size; i++) {
 
-                if (prevTab) {
-                    String tabs = delsemicolon(wordList.get(i));
-                    if (KEY_TABLES.contains(tabs)) {
-                        return "";
+                // previous word = for
+                if (prevFor) {
+                    String[] tabs = delSemicolon(wordList.get(i)).split(",");
+                    for (int s = 0;s<tabs.length;s++) {
+                        if (tabs[s].length()>0) {
+                            if (KEY_TABLES.contains(tabs[s])) {
+                                return "";
+                            } else {
+                                if (i+2>size) {
+                                    qb.buildSubQuery(tabs[s], user_id);
+                                }
+                                else {
+                                    if (wordList.get(i+1).equals("as")) {
+                                        String alias = wordList.get(i+2);
+                                        System.out.println(alias);
+                                        qb.buildSubQueryWithAlias(tabs[s],user_id,alias);
+                                        i+=2;
+                                    }
+                                    else {
+                                        qb.buildSubQuery(tabs[s], user_id);
+                                    }
+                                }
+                            }
+                            if (s < tabs.length - 1) {
+                                qb.buildComma();
+                            }
+                        }
+                    }
+                    if (!ifComma(wordList.get(i))) {
+                        if (i+1<size) {
+                            if (wordList.get(i+1).equals(",")) {
+                                qb.buildComma();
+                                i+=1;
+                            }
+                            else if (wordList.get(i+1).charAt(0)==',') {
+                                qb.buildComma();
+                            }
+                            else {
+                                prevFor = false;
+                            }
+                        }
+                        else {
+                            prevFor = false;
+                        }
                     }
                     else {
-                        qb.buildSubQuery(tabs,user_id);
+                        qb.buildComma();
                     }
-                    prevTab = false;
                 }
+
+
                 else {
                     qb.buildComponent(wordList.get(i));
                 }
 
-
                 if (wordList.get(i).equals("from")) {
-                    prevTab = true;
+                    prevFor = true;
                 }
             }
         }
@@ -57,7 +101,7 @@ public class Filter {
     }
 
     public static void main(String[] args) {
-        System.out.println(filter("select * from salaries;",3L));
+        //System.out.println(filter("select * from salaries where row_id=5",3L));
 
     }
 }
--- src/main/java/com/project/dp/Filter/QueryBuilder.java
+++ src/main/java/com/project/dp/Filter/QueryBuilder.java
@@ -23,6 +23,15 @@ public class QueryBuilder {
         sb.append(" ");
         sb.append(subq);
     }
+    public void buildSubQueryWithAlias(String s,Long user_id,String alias) {
+        String subq = "(select "+s+".* from "+s+" inner join "+ACL_TAB+" on "+ACL_TAB+"."+ROW_ID+"="+s+"."+ROW_ID+
+                " where "+ACL_TAB+"."+ACL_TAB_NAME+"='"+s+"' and "+ACL_TAB+"."+ACL_USER_ID+"="+user_id.toString()+") as "+alias;
+        sb.append(" ");
+        sb.append(subq);
+    }
+    public void buildComma() {
+        sb.append(",");
+    }
     public String build() {
         sb.append(";");
         return sb.toString();
